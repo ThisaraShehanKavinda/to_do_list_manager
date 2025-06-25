5f28129fcc3f79a5d9216de224f43867e0a52905
ThisaraShehanKavinda <thisara2000shehankavinda@gmail.com>
2025-06-25 14:08:06 +0530

    add a logo to header

--- to_do_list/src/components/Header/Header.jsx
+++ to_do_list/src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import {FaUserCircle,FaSignOutAlt} from 'react-icons/fa';
 import './header.css'
+import todoLogo from '../../assets/to-do-list.png'; 
+
 
 const Header = () => {
   return (
-    <div className='Header'>
-      <p className='title'>To-Do List Manager</p>
-      <div className="header-right">
-        <button className='btn profile-btn'>
-            <FaUserCircle className='icon'/> Profile
-        </button>
-        <button className='btn logout-btn'>
-            <FaSignOutAlt className='icon'/> Logout
-        </button>
-      </div>
-    </div>
+<div className='Header'>
+  <div className="header-left">
+    <img src={todoLogo} alt="To-Do Logo" className="logo" />
+    <p className='title'>To-Do List Manager</p>
+  </div>
+
+  <div className="header-right">
+    <button className='btn profile-btn'>
+        <FaUserCircle className='icon'/> Profile
+    </button>
+    <button className='btn logout-btn'>
+        <FaSignOutAlt className='icon'/> Logout
+    </button>
+  </div>
+</div>
   )
 }
 
